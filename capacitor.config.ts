b2eab0f4f7668513116e9412945c722689443c1b
CETECOM <cetecom_conce@duoc.cl>
2024-12-06 13:15:04 -0300

    nombre de apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'uber-duoc',
+  appName: 'FromduocTo',
   webDir: 'www',
   server: {
     androidScheme: 'https'
